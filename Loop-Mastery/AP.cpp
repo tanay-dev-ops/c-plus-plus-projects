c9c112e33c57896e1ef72de153b6db53349705b0
Tanay Mishra <mishratanay93.6@gmail.com>
2026-04-30 12:33:19 +0530

    Create AP.cpp

---
+++ Loop-Mastery/AP.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int x;
+     cout<<"enter a no. :";
+     cin>>x;
+     cout<<"ap of "<<x<<"th term : ";
+     for(int i=1; i<=2*x-1; i=i+2)
+     cout<<i<<" ";
+    }  
