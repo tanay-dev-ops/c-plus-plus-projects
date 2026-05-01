897ac2fcb6537ec3222e4fe0470319fdace79f62
Tanay Mishra <mishratanay93.6@gmail.com>
2026-05-01 19:32:36 +0530

    Create Solid_Rectangle.cpp

---
+++ Pattern-Printing/Solid_Rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter no. of rows :";
+    cin>>x;
+    int y;
+    cout<<"enter no. of columns :";
+    cin>>y;
+    for(int i=1; i<=x; i++)
+   {for(int i=1; i<=y; i++)
+   { cout<<"#";}
+    cout<<endl;}
+   
+}
