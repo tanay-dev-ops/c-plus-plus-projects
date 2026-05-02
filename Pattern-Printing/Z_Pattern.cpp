581c14b1ab610f077f81fc1f63d4d6675183e2cd
Tanay Mishra <mishratanay93.6@gmail.com>
2026-05-02 08:35:16 +0530

    Create Z_Pattern.cpp

---
+++ Pattern-Printing/Z_Pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a no. :";
+    cin>>x;
+    for(int i=1; i<=x; i++){
+        for(int j=1; j<=x; j++){
+            if(i==1 or i==x)
+            cout<<j;}
+            for(int k=1; k<=x; k++){
+                if(i+k==x+1 && k*i!=x)
+                cout<<k;
+            else 
+        cout<<" ";
+    }
+            cout<<endl;}
+}
