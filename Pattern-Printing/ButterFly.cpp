87a0e8f993a8a8462ef8a3950809cc8c1ba935e0
Tanay Mishra <mishratanay93.6@gmail.com>
2026-05-02 08:33:06 +0530

    Create ButterFly.cpp

---
+++ Pattern-Printing/ButterFly.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a no. of rows :";
+    cin>>x;
+    for(int i=1; i<=x; i++){
+        for(int j=1; j<=x; j++){
+           if( j <= i && i + j <= x +1 or j >= i && i + j >= x + 1)
+           cout<<"*";
+           else
+           cout<<" ";}
+            cout<<endl;}
+}
